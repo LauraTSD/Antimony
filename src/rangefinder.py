620ae7c5dc3e13f6b4c05b23a862726c0a317565
laura pircalaboiu <laura.pircalaboiu@yahoo.it>
2022-03-14 10:58:25 +0100

    range finder updated

--- src/rangefinder.py
+++ src/rangefinder.py
@@ -6,8 +6,24 @@ import claripy
 class RangeFinder:
     variable: int
     constraints: List[Constraint]
+    num_values = 100
 
-    def __init__(self):
+    def __init__(self, variable=None, constraints=None):
+        self.variable = variable
+        self.constraints = constraints
 
     def find_values(self):
-        pass
+        s = claripy.Solver()
+        num_values = 100
+
+        for cons in self.constraints:
+            s.add(cons)
+
+        print(s.eval(self.variable, num_values))
+
+
+a = claripy.BVS("sym_val", 32)
+c1 = a < 40
+c2 = a > 30
+r = RangeFinder(a,  [c1, c2])
+r.find_values()
