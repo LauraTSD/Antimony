40863f678198997034eb50bb5a5af2e1b5d57a3f
Laura Ottevanger <laura.ottevanger@hotmail.com>
2022-03-14 11:49:14 +0100

    Created two tests for BNE and LUI

--- test/test_parse.py
+++ test/test_parse.py
@@ -1,6 +1,6 @@
 import unittest
 
-from src.riscv.instructions import Addi, Jal
+from src.riscv.instructions import Addi, Jal, Bneq, Lui
 from src.riscv.parse import parse_instruction
 from src.riscv.registers import RiscvRegister
 
@@ -21,3 +21,15 @@ class Test(unittest.TestCase):
         self.assertEqual(instr.rdest, RiscvRegister.Ra)
         self.assertEqual(instr.immediate, +56)  # offset
 
+    def test_parse_bne(self):
+        "bneq ra, rb, -4"
+        instr = parse_instruction(0b1111111_00110_00111_001_11101_1100011)
+        self.assertIsInstance(instr, Bneq)
+        self.assertEqual(instr.immediate, -4)
+
+    def test_parse_lui(self):
+        "lui ra, 109231"
+        "shift with 12 -> 447410176"
+        instr = parse_instruction(0b00011010101010101111_00111_0110111)
+        self.assertIsInstance(instr, Lui)
+        self.assertEqual(instr.immediate, 447410176)
