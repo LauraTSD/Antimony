d919eecd90ae06b7501224dde61651fb1e3e7cfc
laura pircalaboiu <laura.pircalaboiu@yahoo.it>
2022-03-14 09:55:02 +0100

    start with finding ranges

---
+++ src/rangefinder.py
@@ -0,0 +1,13 @@
+from __future__ import annotations
+
+import os
+import claripy
+
+class RangeFinder:
+    variable: int
+    constraints: List[Constraint]
+
+    def __init__(self):
+
+    def find_values(self):
+        pass
